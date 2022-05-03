decbc8eae14ad2a4e67778fcd2eb0f9b5c7d459b
Addison Hernandez <addison.hernandez@gmail.com>
2022-05-03 16:25:25 -0400

    Add connection to MongoDB

---
+++ db/index.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error(
+    'Please define the MONGO_URI environment variable inside .env.local'
+  );
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose
+      .connect(MONGO_URI, opts)
+      .then((mongoose) => {
+        return mongoose;
+      });
+  }
+
+  console.log('Connecting ...')
+  cached.conn = await cached.promise;
+
+  console.log('Connected to MongoDB')
+
+  return cached.conn;
+}
+
+export default dbConnect;
