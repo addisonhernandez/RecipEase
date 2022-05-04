603bc445e187d2e1dcf0f7a67e2f6eaa7cc6c0df
Addison Hernandez <addison.hernandez@gmail.com>
2022-05-04 14:30:29 -0400

    Add recipe schema

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,5 +1,18 @@
 import mongoose from 'mongoose';
 
 const RecipeSchema = new mongoose.Schema({
-
+  ingredients: {
+    type: [String],
+  },
+  instructions: {
+    type: String,
+  },
+  picture_link: {
+    type: String,
+  },
+  title: {
+    type: String,
+  },
 });
+
+export default mongoose.models.Recipe || mongoose.model('Recipe', RecipeSchema);
