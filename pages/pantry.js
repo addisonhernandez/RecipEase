a1fb81c32124b2d9a3a40573fa9f1b3fd531a829
Addison Hernandez <addison.hernandez@gmail.com>
2022-05-05 10:30:00 -0400

    Add a modal to add an ingredient
    TODO
     - It doesn't work

--- pages/pantry.js
+++ pages/pantry.js
@@ -1,23 +1,53 @@
 import moment from 'moment';
+import { useState } from 'react';
 
 import Layout from '../components/layout';
+import Modal from '../components/Modal';
 import PantryItem from '../components/PantryItem';
+import PantryForm from '../components/PantryForm';
 
 import dbConnect from '../db';
 import Models from '../models';
 
-export default function pantry({ groceries }) {
+export default function Pantry({ groceries }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [newIngredient, setNewIngredient] = useState('');
+
+  const toggle = () => setIsOpen((state) => !state);
+
+  const handleNewIngredient = (ingredient) => {
+    if (!ingredient || !(typeof ingredient === 'string')) {
+      setNewIngredient('');
+      return;
+    }
+
+    console.log({ingredient})
+    fetch('/pantry', {
+      method: 'POST',
+      body: ingredient,
+    }).then(() => setIsOpen(false));
+  };
+
   return (
     <>
       <div>
-        <h1 className='m-0 text-6xl text-center mt-4'>Pantry</h1>
+        <h1 className="m-0 text-6xl text-center mt-4">Pantry</h1>
       </div>
       <Layout>
         {groceries.map((item) => (
           <PantryItem key={item._id} {...item} />
         ))}
-        <button type="button">Add an item</button>
+        <button type="button" onClick={() => setIsOpen(true)}>
+          Add an item
+        </button>
       </Layout>
+      <Modal
+        isOpen={isOpen}
+        toggleModal={toggle}
+        buttonAction={handleNewIngredient}
+      >
+        <PantryForm handleChange={setNewIngredient} />
+      </Modal>
     </>
   );
 }
@@ -32,7 +62,6 @@ export async function getServerSideProps() {
     // fix some weirdness to make them serializable as JSON
     item._id = item._id.toString();
     item.date_added = moment(item.date_added.toString()).format('MMM Do YYYY');
-    item.expiration = moment(item.expiration.toString()).format('MMM Do YYYY');
 
     return item;
   });
