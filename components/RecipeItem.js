9537343958aa34477e766bbb939474b09c7d0b14
Addison Hernandez <addison.hernandez@gmail.com>
2022-05-05 01:43:16 -0400

    Refactor to use detail modal

--- components/RecipeItem.js
+++ components/RecipeItem.js
@@ -1,18 +1,30 @@
+import { useState } from 'react';
+import Modal from './Modal';
+
 import styles from './PantryItem.module.css';
 
-export default function RecipeItem({ title, ingredients }) {
+export default function RecipeItem(props) {
+  const { title, ingredients } = props;
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen((state) => !state);
+
   return (
-    <div className={styles.card}>
-      <h2>{title}</h2>
-      <div>
-        <p>Ingredients: </p>
-        <ul>
-          {ingredients.slice(0, 4).map((ingredient) => (
-            <li key={title + ingredient}>{ingredient}</li>
-          ))}
-          {ingredients.length >= 4 ? <li>...</li> : null}
-        </ul>
+    <>
+      <Modal isOpen={isOpen} recipe={props} toggleModal={toggle}></Modal>
+      <div className={styles.card} onClick={toggle}>
+        <h2>{title}</h2>
+        <div>
+          <p>Ingredients: </p>
+          <ul>
+            {ingredients.slice(0, 4).map((ingredient) => (
+              <li key={title + ingredient}>{ingredient}</li>
+            ))}
+            {ingredients.length >= 4 ? <li>...</li> : null}
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
