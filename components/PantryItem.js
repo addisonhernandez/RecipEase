e11eb872e0710ac0bc6448d34902cc8c6129784a
Addison Hernandez <addison.hernandez@gmail.com>
2022-05-05 10:32:27 -0400

    Add card to show pantry items

--- components/PantryItem.js
+++ components/PantryItem.js
@@ -1,16 +1,11 @@
 import styles from './PantryItem.module.css';
 
-export default function PantryItem({ name, quantity, date_added, expiration }) {
+export default function PantryItem({ ingredient_name, date_added }) {
   return (
     <div className={styles.card}>
-      <h2>{name}</h2>
+      <h2>{ingredient_name}</h2>
       <div>
-        <p>
-          Qty: <span>{quantity.amount}</span>
-          <span>{quantity.unit}</span>
-        </p>
         <p>Date added: {date_added}</p>
-        <p>Expiration: {expiration}</p>
       </div>
     </div>
   );
