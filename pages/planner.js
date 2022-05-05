2661468cd1c685757a90d63eb9c9ea4a7a7a798e
Addison Hernandez <addison.hernandez@gmail.com>
2022-05-04 21:17:30 -0400

    Add a search bar and a <select> element

--- pages/planner.js
+++ pages/planner.js
@@ -1,32 +1,68 @@
 import { useRef, useState } from 'react';
+import Recipe from '../models/Recipe';
+import dbConnect from '../db';
 
-export default function planner() {
+export default function Planner() {
+  const [mealChoices, setMealChoices] = useState();
+
   return (
     <>
       <div>
         <h1 className="m-0 text-6xl text-center mt-4">Meal Plan</h1>
       </div>
-      <div className='grid grid-cols-2 gap-4 mx-8'>
-        <WeekForm />
+      <div className="grid grid-cols-2 gap-4 mx-8">
+        <WeekForm setMealChoices={setMealChoices} />
+        <ShoppingList mealChoices={mealChoices} />
       </div>
     </>
   );
 }
 
+const daysOfWeek = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
+
 const WeekForm = ({ setMealChoices }) => {
   const formRef = useRef();
 
-  const [monday, setMonday] = useState('');
-  const [tuesday, setTuesday] = useState('');
-  const [wednesday, setWednesday] = useState('');
-  const [thursday, setThursday] = useState('');
-  const [friday, setFriday] = useState('');
-  const [saturday, setSaturday] = useState('');
-  const [sunday, setSunday] = useState('');
+  // const [monday, setMonday] = useState('');
+  // const [tuesday, setTuesday] = useState('');
+  // const [wednesday, setWednesday] = useState('');
+  // const [thursday, setThursday] = useState('');
+  // const [friday, setFriday] = useState('');
+  // const [saturday, setSaturday] = useState('');
+  // const [sunday, setSunday] = useState('');
+  const [dayOfWeek, setDayOfWeek] = useState('Monday');
+
+  const handleSubmit = () => {};
+
+  const searchRecipes = async (query) => {
+    await dbConnect();
+
+    const results = Recipe.find();
+  };
 
   return (
-    <form ref={formRef} className="flex flex-col gap-2">
-      <label>
+    <form ref={formRef} className="flex flex-col gap-2" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search for a recipe"
+        onChange={(e) => searchRecipes(e.target.value)}
+      />
+      <select onChange={(e) => setDayOfWeek(e.target.value)}>
+        {daysOfWeek.map((day) => (
+          <option key={day} value={day}>
+            {day}
+          </option>
+        ))}
+      </select>
+      {/* <label>
         <input
           type="text"
           placeholder="Monday"
@@ -74,7 +110,7 @@ const WeekForm = ({ setMealChoices }) => {
           placeholder="Sunday"
           onChange={(e) => setSunday(e.target.value)}
         />
-      </label>
+      </label> */}
     </form>
   );
 };
